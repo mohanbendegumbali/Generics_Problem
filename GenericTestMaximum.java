f0b1f9ec645d693520f9704356ee3f474f90c8c2
mohanbendegumbali <mohan0311@hotmail.com>
2023-02-06 19:50:39 +0530

    Added String Maximum

---
+++ GenericTestMaximum.java
@@ -0,0 +1,41 @@
+
+public class GenericTestMaximum{
+    public Integer testIntegerMaximum(Integer x, Integer y, Integer z) {
+        Integer max=x;
+        if (y.compareTo(max) > 0) {
+             max=y;
+        }
+        if (z.compareTo(max)>0) {
+             max=z;
+        }
+
+        return max;
+
+    }
+
+        public Float testFloatMaximum(Float x, Float y, Float z) {
+        Float max=x;
+        if (y.compareTo(max) > 0) {
+            max=y;
+        }
+        if (z.compareTo(max)>0) {
+            max=z;
+        }
+        return max;
+
+    }
+
+    public String testStringMaximum(String x, String y, String z) {
+        String max=x;
+        if (y.compareTo(max) > 0) {
+            max=y;
+        }
+        if (z.compareTo(max)>0) {
+            max=z;
+        }
+        return max;
+
+    }
+
+
+    }
